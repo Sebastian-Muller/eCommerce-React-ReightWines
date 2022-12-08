e182ab381dc008670335343bdd786dc5bebb8376
Facundo DE LOS RIOS <facujofre96@gmail.com>
2022-12-08 13:05:35 -0300

    Completando estructura de footer con textos e imgs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react"
-
+import Footer from "./components/footer";
 
 function App() {
 
   return (
     <section>
 
+      <Footer/>
     </section>
   );
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,54 +1,62 @@
-
+import logo from "../assets/images/logoNav.webp"
+import location from "../assets/icons/maps.svg"
+import phone from "../assets/icons/telephone.svg"
+import wsp from "../assets/icons/wa.svg"
+import mail from "../assets/icons/mail.svg"
+import cace from "../assets/icons/cace.webp"
+import ssl from "../assets/icons/descarga.webp"
+import dFiscal from "../assets/icons/dataFiscal.png"
+import mPago from "../assets/icons/mercadopago.webp"
 
 
 import React from 'react'
 
-const footer = () => {
+const Footer = () => {
   return (
     <footer>
         <div className="footer__container">
 
             <div className="footer__img">
-                <img src="" alt="" />
+                <img src={logo} alt="" />
             </div>
 
             <div className="footer__contact">
                 <div className="contact__title">
-                    <h3>titulo de seccion</h3>
+                    <h3>Atencion al Cliente</h3>
                 </div>
                 <div className="contact__content">
                     <div className="content__text">
-                        <img src="" alt="" />
-                        <address>Direccion</address>
+                        <img src={location} alt="" />
+                        <p>Av. Las Heras 2440 - C.A.B.A</p>
                     </div>
                     <div className="content__text">
-                        <img src="" alt="" />
-                        <p>telefono</p>
+                        <img src={phone} alt="" />
+                        <a href="tel:+5491148036587">4803 - 6587</a>
                     </div>
                     <div className="content__text">
-                        <img src="" alt="" />
-                        <p>whatsApp</p>
+                        <img src={wsp} alt="" />
+                        <a href="https://wa.me/+5491156298468?text=¡Me+encanto+su+proyecto...+Quiero+sumarme!" target="_blank" >+54 9 11-5629-8468</a>
                     </div>
                     <div className="content__text">
-                        <img src="" alt="" />
-                        <p>email</p>
+                        <img src={mail} alt="" />
+                        <a href="mailto:reight@gmail.com">reight@gmail.com</a>
                     </div>
                 </div>
             </div>
 
             <div className="footer__info">
                 <div className="info__title">
-                    <h3>titulo de seccion</h3>
+                    <h3>PARA SABER</h3>
                 </div>
                 <div className="info__content">
                     <div className="content__text">
-                        <a href="#">enlace</a>
+                        <a href="#">Politicas de Privacidad</a>
                     </div>
                     <div className="content__text">
-                        <a href="#">enlace</a>
+                        <a href="#">Términos y Condiciones</a>
                     </div>
                     <div className="content__text">
-                        <a href="#">enlace</a>
+                        <a href="#">Arrepentimiento de Compra</a>
                     </div>
                 </div>
 
@@ -56,26 +64,26 @@ const footer = () => {
 
             <div className="footer__safeZone">
                 <div className="safeZone__title">
-                    <h3>titulo de seccion</h3>
+                    <h3>SITIO SEGURO</h3>
                 </div>
                 <div className="safeZone__content">
                     <div className="content__payments">
                         <div className="payment">
-                            <img src="#" alt="" />
+                            <img src={cace} alt="cama argentina de comercio electronico" />
                         </div>
                         <div className="payment">
-                            <img src="#" alt="" />
+                            <img src={ssl} alt=" certificado ssl" />
                         </div>
                         <div className="payment">
-                            <img src="#" alt="" />
+                            <img src={dFiscal} alt="data fiscal" />
                         </div>
                         <div className="payment">
-                            <img src="#" alt="" />
+                            <img src={mPago} alt="mercado pago" />
                         </div>
                     </div>
                     <div className="content__text">
                         <h4>Paga en cuotas</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, voluptas?</p>
+                        <p>a traves de todos los medios de pagos y promociones bancarias y Mercado Pago</p>
                     </div>
                 </div>
 
@@ -87,4 +95,4 @@ const footer = () => {
   )
 }
 
-export default footer
+export default Footer
