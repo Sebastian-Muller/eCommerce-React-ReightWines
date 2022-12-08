dcdec6d1f97d30a98d3da9687d4f3c158306a3f3
Facundo DE LOS RIOS <facujofre96@gmail.com>
2022-12-08 11:31:10 -0300

    creaacion de la estructura incial componente footer

---
+++ src/components/footer.js
@@ -0,0 +1,90 @@
+
+
+
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer>
+        <div className="footer__container">
+
+            <div className="footer__img">
+                <img src="" alt="" />
+            </div>
+
+            <div className="footer__contact">
+                <div className="contact__title">
+                    <h3>titulo de seccion</h3>
+                </div>
+                <div className="contact__content">
+                    <div className="content__text">
+                        <img src="" alt="" />
+                        <address>Direccion</address>
+                    </div>
+                    <div className="content__text">
+                        <img src="" alt="" />
+                        <p>telefono</p>
+                    </div>
+                    <div className="content__text">
+                        <img src="" alt="" />
+                        <p>whatsApp</p>
+                    </div>
+                    <div className="content__text">
+                        <img src="" alt="" />
+                        <p>email</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="footer__info">
+                <div className="info__title">
+                    <h3>titulo de seccion</h3>
+                </div>
+                <div className="info__content">
+                    <div className="content__text">
+                        <a href="#">enlace</a>
+                    </div>
+                    <div className="content__text">
+                        <a href="#">enlace</a>
+                    </div>
+                    <div className="content__text">
+                        <a href="#">enlace</a>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="footer__safeZone">
+                <div className="safeZone__title">
+                    <h3>titulo de seccion</h3>
+                </div>
+                <div className="safeZone__content">
+                    <div className="content__payments">
+                        <div className="payment">
+                            <img src="#" alt="" />
+                        </div>
+                        <div className="payment">
+                            <img src="#" alt="" />
+                        </div>
+                        <div className="payment">
+                            <img src="#" alt="" />
+                        </div>
+                        <div className="payment">
+                            <img src="#" alt="" />
+                        </div>
+                    </div>
+                    <div className="content__text">
+                        <h4>Paga en cuotas</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis, voluptas?</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </footer>
+  )
+}
+
+export default footer
