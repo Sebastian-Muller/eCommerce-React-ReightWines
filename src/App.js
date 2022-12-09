33e1fef79226c7518ee746756f33451828974cba
Graciela Faria <gracielafaria94@gmail.com>
2022-12-09 09:22:18 -0300

    app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
+import CardList from "./components/CardList";
+
 
 function App() {
 
   return (
-    <section>
-
+    <section style={{display:'flex', justifyContent:'center'}}>
+        <CardList></CardList>
     </section>
   );
 }
