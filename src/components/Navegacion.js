16dd710f031795e26e1eabb3331db30dae99fd8c
FlorStaricco <florenciastaricco@gmail.com>
2022-12-11 20:45:03 -0300

    boton nav

--- src/components/Navegacion.js
+++ src/components/Navegacion.js
@@ -1,9 +1,8 @@
+
+
 import { NavLink } from "react-router-dom"
-
-
-
-import { Link, NavLink } from "react-router-dom"
 import logoNav from "../assets/images/logoNav.webp"
+import BotonHamburguesa from "./BotonHamburguesa"
 
 
 
@@ -13,18 +12,29 @@ import logoNav from "../assets/images/logoNav.webp"
 const Navegacion = () => {
   return (
     <nav className="navegacion">
-        <img src={logoNav} alt="aaa" height="80px" width="90px"/>
-        <NavLink to="/" className="navItem">Inicio</NavLink>
+
+        <NavLink to="/" className="navItem"><img src={logoNav} alt="Reight" height="80px" width="90px"/></NavLink>
         <NavLink to="/bebidas" className="navItem">Bebidas</NavLink>
         <NavLink to="/nosotros" className="navItem">Nosotros</NavLink>
         <NavLink to="/gift" className="navItem">Gift</NavLink>
         <NavLink to="/contacto" className="navItem">Contacto</NavLink>
-        <NavLink to="/carro"><i class="fa-solid fa-cart-shopping"></i></NavLink>
+        <NavLink to="/carro"><i class="fa-solid fa-magnifying-glass"></i></NavLink>
         <NavLink to="/login"><i class="fa-solid fa-user"></i></NavLink>
+        <NavLink to="/carro"><i class="fa-solid fa-cart-shopping"></i></NavLink>
+          
+      <div className="hamburguesa">
+         <BotonHamburguesa/>
+      </div>
+        
     </nav>
   )
 }
 
 export default Navegacion
+
+
+
+
+
 
 
