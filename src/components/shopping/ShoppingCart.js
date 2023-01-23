7c718af74f173d2ef2fdc58b5076302739953601
Sebastian Müller <94447644+Sebastian-Muller@users.noreply.github.com>
2023-01-23 20:33:43 -0300

    Update ShoppingCart.js

--- src/components/shopping/ShoppingCart.js
+++ src/components/shopping/ShoppingCart.js
@@ -1,45 +1,57 @@
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import { ProductsContext } from '../../context/ProductsProvider'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import CartItem from "./CartItem"
 import {HiOutlineShoppingCart} from 'react-icons/hi';
+import {GiCheckMark} from 'react-icons/gi';
+import { useState } from 'react';
+import { Link } from "react-router-dom"
+
+
 
 
 const ShoppingCart = () => {
- 
-  const {clearCart, delFromCart, cart, state} = useContext(ProductsContext);
-  
-  const costoTotal = cart.reduce((acumulador, object) => {
-    return acumulador + object.precio * object.quantity;
-  }, 0);
 
+  const {clearCart, state} = useContext(ProductsContext);
 
 
   return (
-    <CarritoContainer>
-        <FondoBlanco>
-          <Titulo>
-            <HiOutlineShoppingCart style={{fontSize:35, margin:"0px 18px", color: "var(--orange75)"}}/>
+	@@ -23,38 +23,23 @@ const ShoppingCart = () => {
             <h1>Carrito de Compras</h1>
           </Titulo>
 
-          <div>
-              {cart.map((item, i) => <CartItem key={i} item={item} delFromCart={delFromCart} />)}
-          </div>
-          <div>Total: ${costoTotal}</div>
-          <ButtonsContainer>
-              <ButtonLimpiarCarro onClick={() => clearCart()}>Limpiar Carrito</ButtonLimpiarCarro>
-
-              <ButtonFinalizarCompra>Finalizar Compra</ButtonFinalizarCompra>
-          </ButtonsContainer>
+          <ContainerText>
+            {!state.cart[0] 
+              ? (<p className="empty" >Tu carrito esta vacío, pero puedes ir a ver nuestros productos aquí!</p>) 
+              : state.cart.map((item) => <CartItem key={item.id} item={item} />)
+            }
+            {!state.cart[0] 
+              ? (<Link to="/bebidas" className="modCart" style={{padding:"50px"}}>
+                  <ButtonComprando ><GiCheckMark style={{position:"relative", bottom: "2px"}} /> Seguir Comprando </ButtonComprando>
+                </Link>)
+              : (<ButtonsContainer>
+                  <ButtonLimpiarCarro onClick={() => clearCart()}>Limpiar Carrito</ButtonLimpiarCarro>  
+                  <ButtonFinalizarCompra>Finalizar Compra</ButtonFinalizarCompra>
+                </ButtonsContainer>)
+            }
+          </ContainerText>
         </FondoBlanco>
       </CarritoContainer>
   )
 }
 export default ShoppingCart
- 
 
+
+const fadeIn = keyframes`
+0% {
+    opacity:0;
+}
+100% {
+    opacity:1
+}
+`
 const CarritoContainer = styled.div`
+  animation: ${fadeIn} 0.9s ease-out;
   width: 100%;
   height: 100%;
   display: flex;
@@ -48,8 +60,6 @@ const CarritoContainer = styled.div`
   background: rgb(251, 243, 232);
   gap: 50px;
 `;
-
-
 
 const FondoBlanco = styled.div` width: 80vw;
   height: 90%;
@@ -67,7 +77,6 @@ const FondoBlanco = styled.div` width: 80vw;
   margin-bottom: 10%;
   margin-top: 50px;
 `
-
 const Titulo = styled.div`
   width: 90%;
   height: 50px;
@@ -84,14 +93,43 @@ h1{
   font-weight: 600;
   font-family:'EB Garamond', serif;
 }
- `
+`
+const ContainerText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+const ButtonComprando = styled.button`
+  padding: 5px;
+  border-radius: 0.5rem;
+  border: 1px solid var(--orange);
+  color: var(--orange) ;
+  background-color: var(--beige);
+  font-family: "Nunito", sans-serif;
+  font-size: 12px;
+  text-align: center;
+  width:auto;
+  height: 28px;
+  box-shadow: 0 8px 8px 0 rgba(0,0,0,0.2), 0 6px 12px 0 rgba(0,0,0,0.19);
+@media screen and (min-width: 1024px){
+&:hover{background-color: #999898 !important;
+  color: var(--beige) ;
+  border: 1px solid #999898;}
+&:active {
+  background-color: var(--orange) !important;
+  box-shadow: none;
+  transform: translateY(4px);
+  display: flex;}
+  }
+`
 const ButtonsContainer = styled.div`
   height: 30%;
   text-align: center;
   display: flex;
   flex-direction: row;
   justify-content:space-evenly;
-  gap: 20px; padding-top: 5px;
+  gap: 20px;
+  padding-top: 5px;
   margin-bottom: 25px;
   @media screen and (min-width: 1200px){
     gap: 150px;
@@ -111,10 +149,8 @@ const ButtonLimpiarCarro = styled.button` width: 200px;
   width:auto;
   height: 35px;
   box-shadow: 0 8px 8px 0 rgba(0,0,0,0.2), 0 6px 12px 0 rgba(0,0,0,0.19);
-
   @media screen and (min-width: 1024px){
   &:hover{background-color: #999898 !important;}}
-
   &:active {
   background-color: var(--orange) !important;
   box-shadow: none;
@@ -135,13 +171,11 @@ const ButtonFinalizarCompra = styled.button`
   width:auto;
   height: 35px;
   box-shadow: 0 8px 8px 0 rgba(0,0,0,0.2), 0 6px 12px 0 rgba(0,0,0,0.19);
-
 @media screen and (min-width: 1024px){
 &:hover{
   background-color: #999898 !important;
   color: var(--beige) ;
   border: 1px solid #999898;}
-
 &:active {
   background-color: var(--orange) !important;
   box-shadow: none;
@@ -149,7 +183,6 @@ const ButtonFinalizarCompra = styled.button`
   display: flex;}
   }
 `
-
 // const CounterContainer = styled.div`
 //   height: 20%;
 //   text-align: center;
@@ -159,10 +192,8 @@ const ButtonFinalizarCompra = styled.button`
 //   gap: 0.3px;
 //   padding-top: 15px;
 // `;
-
 /******CODIGO ANTERIOR ********/
 // const ShoppingCart = () => {
-
   // const {clearCart, state} = useContext(ProductsContext);
   
   //   // useEffect(() => {
